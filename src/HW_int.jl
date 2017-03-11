a0a9b06d666d8fb1ab6a7a5450e8997855f90c1b
keynesparis6 <eskenazi.mikael@gmail.com>
2017-03-11 18:50:30 +0100

    tentative answers

--- src/HW_int.jl
+++ src/HW_int.jl
@@ -2,8 +2,19 @@
 module HW_int
 
 
-	# question 1 b) 
-	# here are the packages I used
+#Q1.A:
+global plotb = Gadfly.plot(layer(demand,0,5),Guide.xlabel("Prices"), Guide.ylabel("Quantities"), Guide.Title("Demand function"))
+
+#Change in surplus is
+# /int_1^inf(q(p))-/int_4^inf(q(p))
+# = /int_1^inf(q(p))+/int_inf^4(q(p))
+# = /int_1^4(q(p))
+# = (4*sqrt(p))_1^4
+# = 4
+	
+
+# question 1 b) 
+# here are the packages I used
 
 	using FastGaussQuadrature
 	using Roots
@@ -11,18 +22,43 @@ module HW_int
 	using PyPlot
 	using Distributions
 
-	# here are some functions I defined for useage 
-	# in several sub questions
+# here are some functions I defined for useage 
+# in several sub questions
 
-	# demand function
+#demand function
+	
+p1 = 4
+p2 = 1
 
-	# gauss-legendre adjustment factors for map change
+function demand(p)
+return 2*(p)^(-0.5)
+end
 
-	# eqm condition for question 2
-	# this is the equilibrium condition: total demand = supply, 
+	 
+#Q1.B(1)
+n=10 #test with 100 and 1000
+nodes_start = gausslegendre(n)[:1]
+weights = gausslegendre(n)[:2]
+nodes = nodes_start*(p1-p2)/2 + (p1+p2)/2
+GL = (p1 - p2)/2 * dot(weights, demand.(nodes)) ##finds 4 here again
 
+#Q1.B(2)
+a=1
+b=4
+q(p) = 2/sqrt(p)
+xnodes = rand(Uniform(a,b),n)
+ynodes = map(q, xnodes)
+MC = ((b-a)/n)*sum(ynodes) #approx. 4 here.
 
-	# weighted sum for integration from the slides.
+#Q1.B(3)
+sobol = SobolSeq(1,a,b)
+skip(sobol, n)
+xnodes = hcat([next(sobol) for i = 1:n]...)
+ynodes = map(q,xnodes)
+PseudoMC = ((b-a)/n)*sum(ynodes) #finds 4 here too. 
+
+#Part 2
+couldnt' do it
 
 
 
